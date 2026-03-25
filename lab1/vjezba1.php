024599198e6b88a3ae80d6badf775083de016b30
Ema Čurla <ecurla@tvz.hr>
2026-03-25 14:00:13 +0100

    Add files via upload

---
+++ lab1/vjezba1.php
@@ -0,0 +1,10 @@
+<?php
+$naslov = "Moj prvi PHP dokument";
+$autor = "Ema Čurla";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradila je <strong>$autor</strong>.</p>";
+echo '<a href="https://www.google.com" target="_blank">Posjeti Google</a>';
+?>
+
+<!-- Naziv datoteke: vjezba1.php -->
